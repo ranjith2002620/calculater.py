718a80d31e2b90c00c83fe358079df888190ac4a
ranjith2002620 <ranjoosranju123@gmail.com>
2024-11-22 14:51:45 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,21 @@
+# num1 = int(input("enter first number :"))
+# num2 = int(input("enter second number :"))
+
+# print('1 for addition')
+# print('2 for substration')
+# print('3 for multiplication')
+# print('4 for division')
+
+# choice = int(input("enter a choice:"))
+# print('num1 :',num1,'num2:', num2,'choice :',choice)
+
+# if choice == 1:
+#     print(num1+num2)
+# elif choice == 2:
+#     print(num1-num2)
+# elif choice == 3:
+#     print(num1*num2)
+# elif choice == 4:
+#     print(num1/num2)
+# else:
+#     print("enter something between 1 to 4")
